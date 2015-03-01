005533810433f12e784244fcc2555107732c43c6
Brian Sexton <brian.sexton@gmail.com>
2015-02-28 23:58:38 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Clever Video
+
+**Clean and Fast Video Embedding for Web Pages**
+
+## Overview
+
+Clever Video is a new work in progress. I will update the readme file as I update the project.
+
+## Who is making this?
+
+Clever Video is being made by [Brian Sexton](http://briansexton.com/) of [Carroket, Inc.](http://carroket.com/)
---
+++ clever-video.js
@@ -0,0 +1,65 @@
+/*============================================================================*\
+
+	JavaScript for Clever Video
+	-----------------------------------------------------------------------
+	© 2015 by Carroket, Inc.
+	http://www.carroket.com/
+	-----------------------------------------------------------------------
+	Made by Brian Sexton.
+	http://www.briansexton.com/
+
+ \*============================================================================*/
+
+
+(function(document, angular, options) {
+
+	var cleverVideo = new CleverVideo();
+
+	function CleverVideo() {
+
+		// TO DO: Consider whether any options should be addressed here.
+	}
+
+	// Let's keep our HTML5 valid. OR DIE TRYING.
+
+	if (document.registerElement) {
+
+		document.registerElement("clever-video");
+	}
+
+	// Create an AngularJS module.
+
+	angular.module("cleverVideoModule", [])
+
+		// Now let's paint a happy little directive.
+
+		.directive("clever-video", function() {
+
+			return {
+
+				// Note: templateUrl seems to be relative to the element
+				// context. Yuck.
+
+				// Also Note: The following path does not currently exist
+				// anywhere in the library project.
+
+				// TO DO: Try to make this more flexible without resorting to an absolute path, which could be problematic for non-root site deployment.
+				templateUrl: "_assets/modules/clever-video/video.html",
+
+				replace: true,
+
+				controller: function() {
+
+					console.log("Hello from the clever-video directive controller!");
+				}
+			};
+		});
+
+	// If a namespace was specified, attach cleverVideo to it.
+
+	if (options instanceof Object && options.namespace instanceof Object) {
+
+		options.namespace.cleverVideo = cleverVideo;
+	}
+
+})(window.document, window.angular);
