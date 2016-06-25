9aa24e2bc8e6d094168c2ba939d6a7c74c6e73c1
Brian Sexton <brian.sexton@gmail.com>
2016-06-25 00:14:13 -0700

    Strip package.json and Gruntfile.js of Grunt plugins and tasks because grunt-serve seems to be blocking "npm install".

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,36 +2,8 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 
-		pkg: grunt.file.readJSON('package.json'),
-
-		open: {
-			demo: {
-				path: 'http://localhost:<%= serve.options.port %>/video.html'
-			}
-		},
-
-		serve: {
-			options: {
-				port: 9000,
-				serve: {
-					path: 'build/client/demo'
-				}
-			}
-		},
-
-		watch: {
-			gruntfile: {
-				files: '<%= jshint.gruntfile.src %>',
-				tasks: ['jshint:gruntfile']
-			}
-		}
+		pkg: grunt.file.readJSON('package.json')
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-open');
-	grunt.loadNpmTasks('grunt-serve');
-
-	grunt.registerTask('default', ['demo']);
-
-	grunt.registerTask('demo', ['open:demo', 'serve']);
+	grunt.registerTask('default', []);
 };
